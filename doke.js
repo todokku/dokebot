019cb03df6d3a8d496c4852a1e4d831c00b82c93
KyanMarcos <40499305+KyanMarcos@users.noreply.github.com>
2018-09-20 00:13:49 -0300

    Update doke.js

--- doke.js
+++ doke.js
@@ -13,10 +13,6 @@ client.on('guildMemberAdd', member => {
 <:poritos:475692349645783041> <a:azzz:475694148658266112> Leia as <#466060461335379968> e seja 2000% framily cremely <:monetizado:399274284880494602><:dokefingerguns:445312340682866708>`);
 });
 
-client.on('message', msg => {
-if (msg.content === 'klb kyan') {
-    msg.channel.send('?kick <@299654473054158859> kk eae man 😎');
-  }
-});
+
 
 client.login(process.env.BOT_TOKEN);
