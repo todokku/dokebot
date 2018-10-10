cf8190bb54f0b6e1b0b5b33f7e940fdba77d8069
KyanMarcos <40499305+KyanMarcos@users.noreply.github.com>
2018-10-10 00:40:04 -0300

    Update doke.js

--- doke.js
+++ doke.js
@@ -10,7 +10,7 @@ client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.find(ch => ch.name === '🍆jeral');
     if (!channel) return;
     channel.send(`<a:ztemMuns:461656598197305347>👉 ${member} bem vindo ao servre <a:zbongocat2:471461208394956810>
-<:poritos:475692349645783041> <a:azzz:475694148658266112> Leia as <#466060461335379968> e seja 2000% framily cremely <:monetizado:399274284880494602><:dokefingerguns:445312340682866708>`);
+Leia as <#466060461335379968> e clique no link para ser verificado e poder usar os chats <:monetizado:399274284880494602><:dokefingerguns:445312340682866708>`);
 });
 
 client.login(process.env.BOT_TOKEN);
