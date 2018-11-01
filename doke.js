4b6a2295ec36c1f78f12e18c8867a683521dc2a3
KyanMarcos <40499305+KyanMarcos@users.noreply.github.com>
2018-11-01 00:30:22 -0300

    Update doke.js

--- doke.js
+++ doke.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`6tou!!!11 ${client.users.size} Usuarios.`); 
-  client.user.setPresence({ game: { name: 'October Poop 💩', type: 2, url: 'https://www.youtube.com/channel/UCJMy6zh6BUEK4hydRlbuQeg'} });
+  client.user.setPresence({ game: { name: 'É natal é natal pega no meu...', type: 2, url: 'https://www.youtube.com/channel/UCJMy6zh6BUEK4hydRlbuQeg'} });
 });
 
 client.on('guildMemberAdd', member => {
