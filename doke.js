03cf465d459fcee8bba70cd663eff65875de6e29
kyan <40499305+KyanMarcos@users.noreply.github.com>
2019-04-21 13:21:24 -0300

    Update doke.js

--- doke.js
+++ doke.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 });
 
 client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.find(ch => ch.name === '💕boas-vinda');
+    const channel = member.guild.channels.find(ch => ch.id === '546906263494066176');
     if (!channel) return
     channel.send(`👉 Olá ${member} , seja bem-vindo ao server!
 <a:asleepkirby:532963176883421186> Leia as <#466060461335379968> e não seja um <@&475081215683067904>
